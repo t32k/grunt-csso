2bc1cdd4c0e3242d65ba160780055b27bbb41861
Brad Dougherty <me@brad.is>
2013-02-04 01:32:12 -0500

    Remove extra spaces

--- tasks/csso.js
+++ tasks/csso.js
@@ -108,7 +108,7 @@ module.exports = function (grunt) {
             gzipSize = String(getGzip(mbuf).length).green;
         grunt.log.writeln(' File "' + fileName + '" created.');
         grunt.log.writeln(' Uncompressed size: ' + origSize + ' bytes.');
-        grunt.log.writeln(' Compressed size: ' + gzipSize + ' bytes gzipped ( ' + minSize + ' bytes minified ).');
+        grunt.log.writeln(' Compressed size: ' + gzipSize + ' bytes gzipped (' + minSize + ' bytes minified).');
     };
 
 };
